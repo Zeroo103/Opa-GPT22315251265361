92cde7f40517a26868ec8e42fa842c073f3781e7
Zeroo103 <rbnhoda1@gmail.com>
2025-09-09 22:28:38 +0200

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  distDir: 'out',
+  basePath: process.env.NODE_ENV === 'production' ? '/OpaGPTV21000312' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/OpaGPTV21000312/' : '',
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true
+  }
+}
+
+export default nextConfig
